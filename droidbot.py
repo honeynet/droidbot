a8bb8a4ccf3bc69ba27a344d08ad68e22d3afb5b
Yuanchun Li <pkulyc@gmail.com>
2015-06-10 23:30:02 +0800

    fix argparser

--- droidbot.py
+++ droidbot.py
@@ -5,6 +5,7 @@
 __author__ = 'liyc'
 import sys
 import argparse
+from argparse import RawTextHelpFormatter
 from app_env import AppEnvManager
 from app_event import AppEventManager
 
@@ -43,39 +44,49 @@ def parse_args():
     parse command line input
     generate options including host name, port number
     """
-    usage = "python droidbot.py -p <package name> -n <number of event> " \
-            "[-env <env policy>] [-event <event policy>] [-d <output directory>]\n" \
-            "\t-p\tpackage name of target app\n" \
-            "\t-n\tnumber of event to generate during testing\n" \
-            "\t-env\tenvironment policy to use before app running\n" \
-            "\t\tnone\tno environment will be set. App will run in default environment of device\n" \
-            "\t\tdummy\tadd some fake contacts, SMS log, call log\n" \
-            "\t\tstatic\tset environment based on static analysis result\n" \
-            "\t\t<file>\tget environment policy from a json file\n" \
-            "\t-event\tpolicy of sending events during app running\n" \
-            "\t\tnone\tno event will be sent\n" \
-            "\t\tmonkey\tpseudo-random events, same as \"adb shell monkey ...\"\n" \
-            "\t\tstatic\tsend events based on static analysis result\n" \
-            "\t\tdynamic\tsend events based on dynamic app state, this policy requires framework instrumented\n" \
-            "\t\t<file>\tget event policy from a json file\n" \
-            "\t-d\tdirectory to dump env and event json file\n" \
-            "eg. python droidbot.py -p com.android.calendar -n 500 -env none -event monkey"
-    parser = argparse.ArgumentParser(description="start a robot to interact with Android app")
-    parser.add_argument("-p", action="store", dest="package", nargs=1,
-                      type="string", help="package name")
+    # usage = "python droidbot.py -p <package name> -n <number of event> " \
+    #         "[-env <env policy>] [-event <event policy>] [-d <output directory>]\n" \
+    #         "\t-p\tpackage name of target app\n" \
+    #         "\t-n\tnumber of events to generate during testing\n" \
+    #         "\t-env\tenvironment policy to use before app running\n" \
+    #         "\t\tnone\tno environment will be set. App will run in default environment of device\n" \
+    #         "\t\tdummy\tadd some fake contacts, SMS log, call log\n" \
+    #         "\t\tstatic\tset environment based on static analysis result\n" \
+    #         "\t\t<file>\tget environment policy from a json file\n" \
+    #         "\t-event\tpolicy of sending events during app running\n" \
+    #         "\t\tnone\tno event will be sent\n" \
+    #         "\t\tmonkey\tpseudo-random events, same as \"adb shell monkey ...\"\n" \
+    #         "\t\tstatic\tsend events based on static analysis result\n" \
+    #         "\t\tdynamic\tsend events based on dynamic app state, this policy requires framework instrumented\n" \
+    #         "\t\t<file>\tget event policy from a json file\n" \
+    #         "\t-d\tdirectory to dump env and event json file\n" \
+    #         "eg. python droidbot.py -p com.android.calendar -n 500 -env none -event monkey"
+    parser = argparse.ArgumentParser(description="start a robot to interact with Android app",
+                                     formatter_class=RawTextHelpFormatter)
+    parser.add_argument("-p", action="store", dest="package_name", nargs=1,
+                        required=True, help="package name of target app")
     parser.add_argument("-n", action="store", dest="number", nargs=1,
-                      type="int", help="number of events")
-    parser.add_argument("-env", action="store", dest="env_policy", nargs=1,
-                      type="string", help="policy to set up environment")
-    parser.add_argument("-event", action="store", dest="event_policy", nargs=1,
-                      type="string", help="policy to generate events")
-    parser.add_argument("-d", action="store", dest="directory", nargs=1,
-                      type="string", help="directory of output")
-    options, args = parser.parse_args()
-    if len(args) > 0:
-        parser.error("command error")
-    if not (options.host and options.port):
-        parser.error("command error")
+                        required=True, type=int, help="number of events to generate during testing")
+    parser.add_argument("-env", action="store", dest="env_policy", nargs='?',
+                        help="policy to set up environment. Supported policies:\n"
+                        "none\tno environment will be set. App will run in default environment of device; \n"
+                        "dummy\tadd some fake contacts, SMS log, call log; \n"
+                        "static\tset environment based on static analysis result; \n"
+                        "<file>\tget environment policy from a json file.\n")
+    parser.add_argument("-event", action="store", dest="event_policy", nargs='?',
+                        help="policy to generate events. Supported policies:\n"
+                        "none\tno event will be sent; \n" \
+                        "monkey\tpseudo-random events, same as \"adb shell monkey ...\"; \n" \
+                        "static\tsend events based on static analysis result; \n" \
+                        "dynamic\tsend events based on dynamic app state,"
+                        " this policy requires framework instrumented; \n" \
+                        "<file>\tget event policy from a json file; \n")
+    parser.add_argument("-d", action="store", dest="directory", nargs='?',
+                        help="directory of output")
+    parser.print_help()
+    options = parser.parse_args()
+    print options
+    print vars(options)
     return options
 
 
@@ -87,6 +98,7 @@ def main(opts):
     :param opts: the options parsed by parse_args()
     """
     return
+
 
 if __name__ == "__main__":
     opts = parse_args()

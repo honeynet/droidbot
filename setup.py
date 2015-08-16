907b585ea33fb4f87c101701854069dde084a2b3
Yuanchun <pkulyc@gmail.com>
2015-08-16 17:43:25 +0800

    add setup.cfg and LICENSE

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 # set up basic requirements for droidbot
 __author__ = 'liyc'
 
-from setuptools import setup
-import os
-
+from distutils.core import setup
 setup(
-    name='droidbot',
-    version='0.0.1',
-    description='''
-        A robot which automatically interacts with app in Droidbox.
-        ''',
-    author='Lynn',
-    author_email='pkulyc@gmail.com',
-    install_requires=['']
+  name = 'droidbot',
+  packages = ['droidbot'], # this must be the same as the name above
+  version = '1.0.1',
+  description = 'A smart Android app exerciser.',
+  author = 'Lynn',
+  author_email = 'pkulyc@gmail.com',
+  url = 'https://github.com/lynnlyc/droidbot', # use the URL to the github repo
+  download_url = 'https://github.com/lynnlyc/droidbot/tarball/1.0.1', # I'll explain this in a second
+  keywords = ['testing', 'monkey', 'exerciser'], # arbitrary keywords
+  classifiers = [],
 )

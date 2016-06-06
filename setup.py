659cbb77ca956b133e523bb523b681f70ecd96b7
Yuanchun <pkulyc@gmail.com>
2016-06-06 21:23:27 +0800

    slight change in setup.py

--- setup.py
+++ setup.py
@@ -2,41 +2,43 @@
 __author__ = 'liyc'
 
 from setuptools import setup, find_packages
+
 setup(
-  name = 'droidbot',
-  packages = find_packages(exclude=['docker', 'resources', 'droidbot/tests'
-                                    'droidbot_out', 'evaluation_reports', ]), # this must be the same as the name above
-  version = '1.0.0a2',
-  description = 'A smart Android app exerciser.',
-  author = 'Lynn',
-  license='MIT',
-  author_email = 'pkulyc@gmail.com',
-  url = 'https://github.com/ylimit/droidbot', # use the URL to the github repo
-  download_url = 'https://github.com/ylimit/droidbot/tarball/1.0.0a2',
-  keywords = ['testing', 'monkey', 'exerciser'], # arbitrary keywords
-  classifiers=[
-    # How mature is this project? Common values are
-    #   3 - Alpha
-    #   4 - Beta
-    #   5 - Production/Stable
-    'Development Status :: 3 - Alpha',
+    name='droidbot',
+    packages=find_packages(exclude=['docker', 'droidbot/tests',
+                                    'droidbot_out', 'evaluation_reports', ]),
+    # this must be the same as the name above
+    version='1.0.0a2',
+    description='A smart Android app exerciser.',
+    author='Yuanchun Li',
+    license='MIT',
+    author_email='pkulyc@gmail.com',
+    url='https://github.com/ylimit/droidbot',  # use the URL to the github repo
+    download_url='https://github.com/ylimit/droidbot/tarball/1.0.0a2',
+    keywords=['testing', 'monkey', 'exerciser'],  # arbitrary keywords
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
 
-    # Indicate who your project is intended for
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Testing',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
 
-    # Pick your license as you wish (should match "license" above)
-    'License :: OSI Approved :: MIT License',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
 
-    # Specify the Python versions you support here. In particular, ensure
-    # that you indicate whether you support Python 2, Python 3 or both.
-    'Programming Language :: Python :: 2.7',
-  ],
-  entry_points={
-    'console_scripts': [
-        'droidbot=droidbot:start',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
     ],
-  },
-  # androidviewclient doesnot support pip install, thus we put the directory in ours
-  install_requires=['androguard', 'androidviewclient'],
+    entry_points={
+        "console_scripts": [
+            'droidbot=droidbot:start',
+        ],
+    },
+    # androidviewclient doesnot support pip install, thus we put the directory in ours
+    install_requires=['androguard', 'androidviewclient'],
 )

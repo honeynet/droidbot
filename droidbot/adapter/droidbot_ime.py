e330d749b6acd4d6fe4d9a2e24dfeb83cbd159ea
Yuanchun Li <pkulyc@gmail.com>
2017-07-13 20:25:00 -0400

    add droidbot IME module

---
+++ droidbot/adapter/droidbot_ime.py
@@ -0,0 +1,100 @@
+# coding=utf-8
+
+import logging
+import socket
+import subprocess
+import time
+import json
+from adapter import Adapter
+
+DROIDBOT_APP_PACKAGE = "io.github.ylimit.droidbotapp"
+IME_SERVICE = DROIDBOT_APP_PACKAGE + "/.DroidBotIME"
+
+
+class DroidBotImeException(Exception):
+    """
+    Exception in telnet connection
+    """
+    pass
+
+
+class DroidBotIme(Adapter):
+    """
+    a connection with droidbot ime app.
+    """
+    def __init__(self, device=None):
+        """
+        initiate a emulator console via telnet
+        :param device: instance of Device
+        :return:
+        """
+        self.logger = logging.getLogger('DroidBotIME')
+        if device is None:
+            from droidbot.device import Device
+            device = Device()
+        self.device = device
+        self.connected = False
+
+    def set_up(self):
+        device = self.device
+        if DROIDBOT_APP_PACKAGE in device.adb.get_installed_apps():
+            self.logger.debug("DroidBot app was already installed.")
+        else:
+            # install droidbot app
+            import pkg_resources
+            droidbot_app_path = pkg_resources.resource_filename("droidbot", "resources/droidbotApp.apk")
+            install_cmd = "install %s" % droidbot_app_path
+            self.device.adb.run_cmd(install_cmd)
+            self.logger.debug("DroidBot app installed.")
+
+    def tear_down(self):
+        self.device.uninstall_app(DROIDBOT_APP_PACKAGE)
+
+    def connect(self):
+        r_enable = self.device.adb.shell("ime enable %s" % IME_SERVICE)
+        if r_enable.endswith("now enabled"):
+            r_set = self.device.adb.shell("ime set %s" % IME_SERVICE)
+            if r_set.endswith("selected"):
+                self.connected = True
+                return
+        self.logger.warning("Failed to connect DroidBotIME!")
+
+    def check_connectivity(self):
+        """
+        check if droidbot app is connected
+        :return: True for connected
+        """
+        return self.connected
+
+    def disconnect(self):
+        """
+        disconnect telnet
+        """
+        self.connected = False
+        r_disable = self.device.adb.shell("ime disable %s" % IME_SERVICE)
+        if r_disable.endswith("now disabled"):
+            self.connected = False
+            print "[CONNECTION] %s is disconnected" % self.__class__.__name__
+            return
+        self.logger.warning("Failed to disconnect DroidBotIME!")
+
+    def input_text(self, text, mode=0):
+        """
+        Input text to target device
+        :param text: text to input, can be unicode format
+        :param mode: 0 - set text; 1 - append text.
+        :return: 
+        """
+        input_cmd = "am broadcast -a DROIDBOT_INPUT_TEXT --es text %s --ei mode %d" % (text, mode)
+        self.device.adb.shell(input_cmd)
+
+if __name__ == "__main__":
+    droidbot_ime_conn = DroidBotIme()
+    droidbot_ime_conn.set_up()
+    droidbot_ime_conn.connect()
+    droidbot_ime_conn.input_text("hello", 0)
+    droidbot_ime_conn.input_text(u"世界!", 1)
+    time.sleep(2)
+    droidbot_ime_conn.input_text(u"再见，world", 0)
+    droidbot_ime_conn.disconnect()
+    droidbot_ime_conn.tear_down()
